c83697f51d2ac27f3b5ede7c004d7cce7dae65ec
Vitaliy Alexandrovich <Dobrota2017@outlook.com>
2019-04-11 19:45:36 +0300

    fds

--- Git/Program.cs
+++ Git/Program.cs
@@ -10,7 +10,7 @@ namespace Git
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("OneTwo");
+            Console.WriteLine("OneTwoThree");
         }
     }
 }
