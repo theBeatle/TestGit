d8aad85b617276210d7917303f90eb23c84a25c8
DESKTOP-EPN9CP6\thebe <oleh.knyaz@gmail.com>
2019-04-11 19:40:48 +0300

    sd;ksdfk;sdf;sd

--- Git/Program.cs
+++ Git/Program.cs
@@ -10,6 +10,11 @@ namespace Git
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("sdsdaasdsa");
+        }
+
+        static void Somemethod()
+        {
         }
     }
 }
